f38358325d4b19b881158064291a57fbaa643439
Anthony Destenay <anthony.destenay@gmail.com>
2023-05-26 12:17:40 +0200

    Added TwitchIO and !test command

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import http.server
 import socketserver
 import threading
 import functools
+from dotenv import dotenv_values
+from twitchio.ext import commands
+
+config = dotenv_values(".env")
+
+bot = commands.Bot(
+    token=config['TMI_TOKEN'],
+    client_id=config['CLIENT_ID'],
+    nick=config['BOT_NICK'],
+    prefix=config['BOT_PREFIX'],
+    initial_channels=[config['CHANNEL']]
+)
 
 
 def start_server():
@@ -17,10 +29,21 @@ def start_server():
         httpd.serve_forever()
 
 
+@bot.event
+async def event_ready():
+    print(f"{config['BOT_NICK']} is online!")
+
+
+@bot.command(name='test')
+async def test(ctx):
+    await ctx.send('test passed!')
+
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
-    threading.Thread(target=start_server).start()
     print_hi('PyCharm')
+    threading.Thread(target=start_server).start()
+    bot.run()
--- readme.md
+++ readme.md
@@ -1,5 +1,15 @@
 # PoC: Twitch Bot & Overlay
 
+Configurer TwitchIO, en créant un fichier `.env` avec :
+
+```
+TMI_TOKEN= # https://twitchapps.com/tmi/
+CLIENT_ID= # Identifiant Twitch de l'app à créer sur https://dev.twitch.tv
+BOT_NICK= # Pseudo du Bot, doit correspondre au compte connecté pour TMI_TOKEN
+BOT_PREFIX= # Préfixe des commandes ("!" par exemple)
+CHANNEL= # Nom de la chaîne
+```
+
 Pour lancer le serveur Python (http://localhost:8000) :
 
 ``py main.py``
