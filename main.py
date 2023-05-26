e5b01cf92669bc993f0922e82f6a4ba5347ba242
Anthony Destenay <anthony.destenay@gmail.com>
2023-05-26 11:09:04 +0200

    Added VueJS 3

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import http.server
 import socketserver
 import threading
+import functools
 
 
 def start_server():
     port = 8765
     handler = http.server.SimpleHTTPRequestHandler
+    handler.extensions_map.update({
+        ".js": "text/javascript",
+    })
+    handler = functools.partial(handler, directory='overlay/dist')
+
     with socketserver.TCPServer(("", port), handler) as httpd:
         print("Server started at localhost:" + str(port))
         httpd.serve_forever()
